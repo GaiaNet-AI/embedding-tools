f0a8033876efb694f8cf1aca33a6873462496e74
Michael Yuan <michael@secondstate.io>
2024-09-01 06:12:59 +0000

    Update utils
    Signed-off-by: Michael Yuan <michael@secondstate.io>

--- csv_embed/README.md
+++ csv_embed/README.md
@@ -72,16 +72,52 @@ You can always delete the vector collection and start over.
 curl -X DELETE 'http://localhost:6333/collections/my_book'
 ```
 
-## Create a CSV file
+## Create a vectors file from a single column CSV file
 
 We start from a single column CSV file. Each row has only one cell, and it contains a complete article or chapter. Let's call it `source_text.csv`.
 
 Run a Python script to add a second column and create a new CSV file `vectors.csv`. The second column contains the summarization and QnA of the source text. It is then converted into vectors using the above `csv_embed.wasm` utility.
 
 ```
-python3 generate_vectors.py source_text.csv vectors.csv
+python3 vectors_from_csv.py source_text.csv vectors.csv
 ```
 
 The script uses the `llama.us.gaianet.network` node to summarize and generate QnA pairs based on the source text. You can switch to a different LLM (e.g., GPT-4) by changing the configuration at the top of the Python file.
+
+Example
+
+```
+curl -LO https://huggingface.co/datasets/gaianet/samsung/resolve/main/samsung-galaxy-s24-source.csv
+python3 vectors_from_csv.py samsung-galaxy-s24-source.csv samsung-galaxy-s24-pairs.csv
+
+
+wasmedge --dir .:. \
+  --nn-preload embedding:GGML:AUTO:nomic-embed-text-v1.5.f16.gguf \
+  csv_embed.wasm embedding samsung_galaxy_s24 768 samsung-galaxy-s24-pairs.csv -c 8192
+```
+
+## Create a vectors file from a text file
+
+We start from a text file. Each paragraph, separated by a blank line, is an article or a chapter. Let's call it `source.txt`.
+
+Run a Python script to create a new CSV file `vectors.csv`. The first column is the source paragraph text and the second column contains the summarization and QnA of the source text. It is then converted into vectors using the above `csv_embed.wasm` utility.
+
+```
+python3 vectors_from_paragraph.py source.txt vectors.csv
+```
+
+The script uses the `llama.us.gaianet.network` node to summarize and generate QnA pairs based on the source text. You can switch to a different LLM (e.g., GPT-4) by changing the configuration at the top of the Python file.
+
+Example
+
+```
+curl -LO https://huggingface.co/datasets/gaianet/learn-rust/resolve/main/rust-books-source.txt
+python3 vectors_from_paragraph.py rust-books-source.txt rust-books-pairs.csv
+
+
+wasmedge --dir .:. \
+  --nn-preload embedding:GGML:AUTO:nomic-embed-text-v1.5.f16.gguf \
+  csv_embed.wasm embedding rust_books 768 rust-books-pairs.csv -c 8192
+```
 
 
---
+++ csv_embed/vectors_from_paragraph.py
@@ -0,0 +1,101 @@
+import openai
+import csv
+import sys
+import os
+
+API_BASE_URL = "https://llama.us.gaianet.network/v1"
+MODEL_NAME = "llama"
+API_KEY = "GAIA"
+
+def summarize(source_text):
+    client = openai.OpenAI(base_url=API_BASE_URL, api_key=API_KEY)
+
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": "Respond with a comprehensive summary of the text in the user message",
+            },
+            {
+                "role": "user",
+                "content": source_text,
+            }
+        ],
+        model=MODEL_NAME,
+        stream=False,
+    )
+    return chat_completion.choices[0].message.content
+
+def qgen(source_text):
+    client = openai.OpenAI(base_url=API_BASE_URL, api_key=API_KEY)
+
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": "Respond with a list of 10 questions. The text in the user message must contain specific answers to each question. Each question must be complete without references to unclear context such as \"this team\" or \"that lab\". Each question must be on its own line. Just list the questions without any introductory text or numbers.",
+            },
+            {
+                "role": "user",
+                "content": source_text,
+            }
+        ],
+        model=MODEL_NAME,
+        stream=False,
+    )
+    return chat_completion.choices[0].message.content
+
+def agen(source_text, question):
+    client = openai.OpenAI(base_url=API_BASE_URL, api_key=API_KEY)
+
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": "Give a comprehensive and well-reasoned answer to the user question strictly based on the context below.\n" + source_text,
+            },
+            {
+                "role": "user",
+                "content": question,
+            }
+        ],
+        model=MODEL_NAME,
+        stream=False,
+    )
+    return chat_completion.choices[0].message.content
+
+def main():
+    results = [];
+    arguments = sys.argv[1:]
+
+    with open(arguments[0], 'r', newline='') as txtfile:
+        txt_data = txtfile.read()
+        paras = txt_data.split('\n\n') 
+
+        for p in paras:
+            print('section start')
+
+            result = []
+            result.append(p)
+            result.append(summarize(p))
+            results.append(result)
+
+            qs = qgen(p)
+            for q in qs.splitlines():
+                if len(q.strip()) == 0:
+                    continue
+                print('question: ' + q)
+                result = []
+                result.append(p)
+                result.append(q + "\n" + agen(p, q))
+                results.append(result)
+
+            print('section end')
+
+    with open(arguments[1], 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        for row in results:
+            writer.writerow(row)
+
+if __name__ == "__main__":
+    main()
